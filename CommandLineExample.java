0a25a7b8a645e32a9776a7915a06ae688efc26df
radheashu <ashwinihanwate1020@gmail.com>
2022-05-09 02:58:12 -0700

    first commit

---
+++ CommandLineExample.java
@@ -0,0 +1,9 @@
+package assignment1;
+
+public class CommandLineExample {
+
+    public static void main(String args[]){
+            System.out.println("Your first argument is: "+args[0]);
+        }
+    }
+
